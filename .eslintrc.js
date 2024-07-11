019c1ba5b9b63ebf9c1591542db14b7f36df2440
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-07-11 19:06:36 +0300

    chore: Update ESLint configuration to include Prettier plugin and enforce Prettier formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
   root: true,
   env: {
     node: true,
