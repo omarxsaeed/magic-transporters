3eac3237f2e360bd2519518ea20368071a3f0085
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-07-11 21:30:12 +0300

    feat: Add MagicItemController for creating magic items

---
+++ src/magic-item/magic-item.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { MagicItemService } from './magic-item.service';
+import { CreateMagicItemDto } from './dto/create-magic-item.dto';
+
+@Controller('magic-item')
+export class MagicItemController {
+  constructor(private readonly magicItemService: MagicItemService) {}
+
+  @Post()
+  create(@Body() createMagicItemDto: CreateMagicItemDto) {
+    return this.magicItemService.createMagicItem(createMagicItemDto);
+  }
+}
