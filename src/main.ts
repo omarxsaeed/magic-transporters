2be91be28d46e804496454f51b7f6bbf5eba3982
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-07-11 18:30:06 +0300

    chore: Set global API prefix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
+  app.setGlobalPrefix('api');
+
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
