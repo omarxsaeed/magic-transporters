f35ea55494e2ba3f95ced8c665cbfd6ae83dcc1d
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-07-11 17:31:37 +0300

    refactor: Update app.module.ts with TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: ['dist/**/*.entity.js'],
+        autoLoadEntities: true,
+        namingStrategy: new SnakeNamingStrategy(),
+        logging: true,
+        logger: 'advanced-console',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
