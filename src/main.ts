5349aceb081b2f0c864d608c75106556af0e3e61
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-07-14 02:36:55 +0300

    feat: Update Swagger documentation setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,27 +9,32 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  app.enableCors();
+
   app.useLogger(app.get(Logger));
 
   const config = new DocumentBuilder()
     .setTitle('Magic Transporters')
     .setDescription('Magic Transporters API Documentations')
     .setVersion('1.0')
+    .addServer(configService.get('AWS_SERVER'))
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document, {
     customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
     ],
+    swaggerOptions: {
+      protocol: 'http',
+    },
   });
 
   app.use(helmet());
-
-  app.enableCors();
-
-  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
