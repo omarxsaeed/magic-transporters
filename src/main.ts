197b72c2147ef15207952ec35f8da9dac9000da7
Omar Saeed <40320750+omarxsaeed@users.noreply.github.com>
2024-07-13 23:13:51 +0300

    feat: Update Swagger documentation setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,13 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   app.use(helmet());
 
